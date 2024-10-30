bcac3fba7f7f3e607b6578a9dbe440ce96160c64
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2024-10-29 21:27:52 -0700

    Update Levitas.java

--- src/main/java/corwin/levitas/Levitas.java
+++ src/main/java/corwin/levitas/Levitas.java
@@ -1,24 +1,22 @@
 package corwin.levitas;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import corwin.levitas.block.LevitasBlock;
+import net.minecraft.block.Block;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Levitas implements ModInitializer {
 	public static final String MOD_ID = "levitas";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Block LEVITAS_BLOCK = new LevitasBlock();
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+        @Override
+        public void onInitialize() {
+            Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "levitas_block"), LEVITAS_BLOCK);
+        }
+    }
 }
