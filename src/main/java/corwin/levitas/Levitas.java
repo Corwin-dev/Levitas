47a73752e439e5bb125cd267c3a2ddbe741f904b
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2024-10-29 22:00:09 -0700

    Update Levitas.java

--- src/main/java/corwin/levitas/Levitas.java
+++ src/main/java/corwin/levitas/Levitas.java
@@ -14,14 +14,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Levitas implements ModInitializer {
-	public static final String MOD_ID = "levitas";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final Block LEVITAS_BLOCK = new LevitasBlock();
+    public static final String MOD_ID = "levitas";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Block LEVITAS_BLOCK = new LevitasBlock();
 
-        @Override
-        public void onInitialize() {
-            Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "levitas_block"), LEVITAS_BLOCK);
-            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "levitas_block"), LEVITAS_BLOCK_ITEM);
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "levitas_block"), LEVITAS_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "levitas_block"), LEVITAS_BLOCK_ITEM);
 	}
-    }
 }
