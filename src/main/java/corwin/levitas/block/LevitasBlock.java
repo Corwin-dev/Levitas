58b2eefd1e102d8761c149b7d05dd27c5a9b189c
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2024-10-29 21:25:49 -0700

    Update LevitasBlock.java

--- src/main/java/corwin/levitas/block/LevitasBlock.java
+++ src/main/java/corwin/levitas/block/LevitasBlock.java
@@ -1,1 +1,15 @@
+package corwin.levitas.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.block.AbstractBlock;
+
+public class LevitasBlock extends Block {
+    public LevitasBlock() {
+        super(AbstractBlock.Settings.of(Material.METAL)
+            .strength(4.0f)  // Adjust the hardness as desired
+            .requiresTool()
+            .sounds(BlockSoundGroup.METAL));
+    }
+}
